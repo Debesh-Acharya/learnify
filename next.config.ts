ad35ddd70d5fe267dc0f747909d569707bebedc1
Debesh-Acharya <debeshacharya30@gmail.com>
2025-05-21 01:57:37 +0530

    vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'b.thumbs.redditmedia.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'a.thumbs.redditmedia.com',
+        pathname: '/**',
+      },
+      // Add other Reddit domains you might encounter
+      {
+        protocol: 'https',
+        hostname: 'preview.redd.it',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.redd.it',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.redditstatic.com',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
